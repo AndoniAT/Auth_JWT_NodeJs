b64c5fe8cfbc21bb15eb52235dd3d1de65f88495
AndoniAT <andonialonsotort@gmail.com>
2025-01-09 23:09:36 +0100

    Fix requite credentials in index

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const authRoute = require( './api/routes/Auth.js' );
 const cors = require( 'cors' );
 const cookieParser = require( 'cookie-parser' );
 const corsOptions = require( './config/corsOptions.js' );
-const credentials = require('./api/middlewares/Credentials.js');
+const credentials = require( './api/middlewares/credentials.js' );
 const app = express();
 
 /*
