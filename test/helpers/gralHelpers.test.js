3cd05b16c9abdf46d55e48589beb9a857904d215
AndoniAT <andonialonsotort@gmail.com>
2025-01-27 18:02:16 +0100

    GralHelpers tests

---
+++ test/helpers/gralHelpers.test.js
@@ -0,0 +1,44 @@
+const { describe, it } = require( 'mocha' );
+const GralHelpers = require( '../../src/api/helpers/GralHelpers' );
+const CustomError = require( '../../src/api/classes/customError' );
+
+describe( '/GralHelpers tests', () => {
+    describe( 'Detect Errors', () => {
+        it( 'Empty object => No Error', () => {
+            ( () => {
+                GralHelpers.detectError( {} );
+            }
+            ).should.be.ok;
+        } );
+
+        it( 'Error message => Status 400', () => {
+            const errObj = { message: 'Error 400' };
+            const fun = ( () => {
+                GralHelpers.detectError( errObj );
+            }
+            );
+            fun.should.throw( CustomError );
+            try {
+                fun();
+            } catch( e ) {
+                e.message.should.contain( JSON.stringify( errObj ) );
+                e.should.have.status( 400 );
+            }
+        } );
+
+        it( 'Error message => Status 500', () => {
+            const errObj = { message: 'Error' };
+            const fun = ( () => {
+                GralHelpers.detectError( errObj, 500 );
+            }
+            );
+            fun.should.throw( CustomError );
+            try {
+                fun();
+            } catch( e ) {
+                e.message.should.contain( JSON.stringify( errObj ) );
+                e.should.have.status( 500 );
+            }
+        } );
+    } );
+} );
